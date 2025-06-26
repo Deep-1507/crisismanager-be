61f70694dcf649abe767bf38dbc9e37f7ced48d3
Deep-1507 <deependrakumar15072003@gmail.com>
2025-06-26 17:23:25 +0530

    *

--- index.js
+++ index.js
@@ -9,11 +9,18 @@ dotenv.config();
 connectDB();
 
 const app = express();
-app.use(cors());
+
+// CORS CONFIGURATION
+app.use(cors({
+  origin: "*", // frontend origin(s)
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true // allow cookies if needed
+}));
+
 app.use(express.json());
 
 app.use("/api/users", userRoutes);
 app.use("/api/location", locationRoutes);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));import express from "express";
